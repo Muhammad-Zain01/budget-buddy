23944f1647b8713cdbb754351f1cc69d2ba695f6
Muhammad Zain <83939353+Muhammad-Zain01@users.noreply.github.com>
2024-09-24 22:26:35 +0500

    Changes

--- src/email/verification.tsx
+++ src/email/verification.tsx
@@ -73,40 +73,24 @@ export const BudgetBuddyVerificationEmail = ({
               target="_blank"
               style={iconLink}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
+              <Img
+                src="https://budget-buddy-v1.vercel.app/github.png"
                 width="20"
                 height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77 5.44 5.44 0 0 0 3.5 8.5c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 0 0-.94 2.61V22"></path>
-              </svg>
+                alt="GitHub"
+              />
             </Link>
             <Link
               href="https://www.linkedin.com/in/muhammad-zain01/"
               target="_blank"
               style={iconLink}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
+              <Img
+                src="https://budget-buddy-v1.vercel.app/linkedin.png"
                 width="20"
                 height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-4 0v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                <rect x="2" y="9" width="4" height="12"></rect>
-                <circle cx="4" cy="4" r="2"></circle>
-              </svg>
+                alt="LinkedIn"
+              />
             </Link>
           </Container>
         </Container>
