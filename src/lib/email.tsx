b4bb75bc8be39b082dcc6ef6aa2a1c5de3fee722
Muhammad Zain <83939353+Muhammad-Zain01@users.noreply.github.com>
2024-09-25 12:05:55 +0500

    Update email.tsx

--- src/lib/email.tsx
+++ src/lib/email.tsx
@@ -4,12 +4,12 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmail = async (to: string, subject: string, react: any) => {
+const sendEmail = async (to: string, subject: string, html: any) => {
   return await resend.emails.send({
     from: `${process.env.EMAIL_TITLE} <${process.env.EMAIL_USER}>`,
     to: to,
     subject: subject,
-    react: react,
+    html: html,
   });
 };
 
@@ -19,7 +19,9 @@ export const sendVerificationEmail = async (
   name: string
 ) => {
   const subject = "Email Verification";
-  const html = <Verification verificationCode={code} name={name} />;
+  const html = await render(
+    <Verification verificationCode={code} name={name} />
+  );
   const response = await sendEmail(to, subject, html);
   console.log(response);
   return response;
