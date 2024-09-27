903adf130480d4c8af198b3ceee150385130f52b
Muhammad Zain <83939353+Muhammad-Zain01@users.noreply.github.com>
2024-09-27 23:07:05 +0500

    Update email.tsx

--- src/lib/email.tsx
+++ src/lib/email.tsx
@@ -2,10 +2,11 @@ import { render } from "@react-email/components";
 import Plunk from "@plunk/node";
 import Verification from "@/email/verification";
 
-// @ts-ignore
-const plunk = new Plunk(process.env.EMAIL_API_KEY);
-
 const sendEmail = async (to: string, subject: string, html: any) => {
+  console.log("EMAIL_KEY", process.env.EMAIL_API_KEY);
+  // @ts-ignore
+  const plunk = new Plunk(process.env.EMAIL_API_KEY);
+
   return plunk.emails.send({
     to: to,
     subject: subject,
